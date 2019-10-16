3ec32f5fc0f16a7c05c7f3e8c204789e164360ed
David Boedigheimer <stealth259@gmail.com>
2019-10-16 15:29:22 -0700

    Add new array and show it through a ul.

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,16 @@
 $(document).ready(function() {
   $("#form-one").submit(function(e) {
     var color = $("input#color").val();
-    var animal = $("input#color").val();
+    var animal = $("input#animal").val();
     var game = $("input#game").val();
     var food = $("input#food").val();
+
+    var total = [color, animal, game, food];
+
+    var newTotal = [];
+    newTotal.push(total[1], total[0], total[2]);
+
+    $("ul#show").prepend("<li>" + newTotal[0] + "</li>", "<li>" + newTotal[1] + "</li>", "<li>" + newTotal[2] + "</li>");
 
     e.preventDefault();
   });
