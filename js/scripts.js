3830c607ab6ee6035691932279e1836a9f80c99f
David Boedigheimer <stealth259@gmail.com>
2019-10-16 14:31:44 -0700

    Add submit function and edit main page.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Backend
+
+// Front end
+$(document).ready(function() {
+  $("#form-one").submit(function(e) {
+    var color = $("input#color").val();
+    var animal = $("input#color").val();
+    var game = $("input#game").val();
+    var food = $("input#food").val();
+
+    e.preventDefault();
+  });
+});
